e1bf7bab88fc2abca98151d77312649b5fcd0f07
John Lee <33932085+JohnLee16@users.noreply.github.com>
2019-12-03 13:53:58 +0800

    change the path
    change the path of  WallLine3DView.csproj and other files

---
+++ WallLine3DView/ReadJSONFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using Newtonsoft.Json;
+using Utilities.BimDataProcess;
+
+namespace WallLine3DView
+{
+    class ReadJSONFile
+    {
+        public bool ReadJSONFileLocal(string jsonPath, out BimWall bimWall)
+        {
+            var jsonwall = File.ReadAllText(jsonPath);
+            bimWall = JsonConvert.DeserializeObject<BimWall>(jsonwall);
+            return true;
+        }
+    }
+}
---
+++ WallLine3DView/WallLine3DViewShow.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HelixToolkit;
+using HelixToolkit.Wpf;
+using System.Windows.Media.Media3D;
+using System.Windows.Media;
+using System.Windows;
+
+namespace WallLine3DView
+{
+    public class WallLine3DViewShow
+    {
+        public WallLine3DViewShow()
+        {
+            var modelGroup = new Model3DGroup();
+            ReadJSONFile readJsonFile = new ReadJSONFile();
+            
+            
+            // Create a mesh builder and add a box to it
+            var meshBuilder = new MeshBuilder(false, false);
+            //meshBuilder.AddBox(new Point3D(0, 0, 0), 1, 2, 0.5);
+            meshBuilder.AddBox(new Rect3D(0, 0, 0, 2, 2, 2));
+            List<Point> contours = new List<Point>() { new Point(0,0), new Point(0,800), new Point(58,800), new Point(58, 700), new Point(1592, 700), new Point(1592, 0), new Point(0, 0),
+            };//new Point3D(0,0,112), new Point3D(0,800,112), new Point3D(58,800,112), new Point3D(58, 700, 112), new Point3D(1592, 700, 112), new Point3D(1592, 0, 112)
+            
+            PointCollection contour = new PointCollection() { new Point(0, 0), new Point(0, 800), new Point(58, 800), new Point(58, 700), new Point(1592, 700), new Point(1592, 0), new Point(0, 0) };
+            Point3DCollection topSurface = new Point3DCollection() { new Point3D(0, 0, 0), new Point3D(0, 800, 0), new Point3D(58, 800, 0), new Point3D(58, 700, 0), new Point3D(1592, 700, 0), new Point3D(1592, 0, 0), new Point3D(0, 0, 0) };
+            var axis = new Vector3D(0, 0, 12);
+            var render = new ExtrudedVisual3D();
+            meshBuilder.AddPolygon(topSurface);
+
+            render.Section = contour;
+            render.Path.Add(new Point3D(0, 0, 0));
+            render.Path.Add(new Point3D(0, 0, 112));
+            render.SectionXAxis = axis;
+            render.Fill = Brushes.Green;
+            render.IsPathClosed = true;
+            render.IsSectionClosed = true;
+
+            //meshBuilder.
+            // Create a mesh from the builder (and freeze it)
+            var mesh = meshBuilder.ToMesh(true);
+            
+            // Create some materials
+            var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
+            var redMaterial = MaterialHelper.CreateMaterial(Colors.Red);
+            var blueMaterial = MaterialHelper.CreateMaterial(Colors.Blue);
+            var insideMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
+
+            // Add 3 models to the group (using the same mesh, that's why we had to freeze it)
+            modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Material = greenMaterial, BackMaterial = insideMaterial });
+            //modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(-2, 0, 0), Material = redMaterial, BackMaterial = insideMaterial });
+            //modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Transform = new TranslateTransform3D(2, 0, 0), Material = blueMaterial, BackMaterial = insideMaterial });
+            render.IsAttachedToViewport3D();
+            // Set the property, which will be bound to the Content property of the ModelVisual3D (see MainWindow.xaml)
+            //this.WallModel = modelGroup;
+            //this.WallModel = modelGroup;
+            
+        }
+        public Model3D WallModel { get; set; }
+        public ExtrudedVisual3D ExtrudedWall { get; set; }
+    }
+}
